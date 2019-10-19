02555572d2e72eebe26b300b1630c8b23acc2835
leomei95 <46854501+leomei95@users.noreply.github.com>
2019-10-19 00:21:50 -0700

    added navbar, about me
    fixed minor issues

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  var scroll_start = 0;
+  var startchange = $("#aboutme");
+  var offset = startchange.offset();
+
+  if (startchange.length) {
+    $(document).scroll(function () {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        $("nav").css('background-color', 'black');
+        $(".navlist > li > a").css('color', 'white');
+
+      } else {
+        $('nav').css('background-color', 'grey');
+        $(".navlist > li > a").css('color', 'black');
+      }
+    });
+  }
+});
+/*add navbar active link*/
