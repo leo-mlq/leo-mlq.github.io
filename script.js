7d8a34a824ab63501aa197aa1603ed323c7f23bc
leomei95 <liqiangmei@outlook.com>
2019-11-19 12:18:31 -0800

    add nav highlight

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-$(document).ready(function () {
+const navColorChange=()=>{
   var scroll_start = 0;
-  var startchange = $("#aboutme");
-  var offset = startchange.offset();
+    var startchange = $("#aboutme");
+    var offset = startchange.offset();
 
-  if (startchange.length) {
-    $(document).scroll(function () {
-      scroll_start = $(this).scrollTop();
-      if (scroll_start > offset.top) {
-        $("nav").css('background-color', 'black');
-        $(".navlist > li > a").css('color', 'white');
-
-      } else {
-        $('nav').css('background-color', 'grey');
-        $(".navlist > li > a").css('color', 'black');
-      }
-    });
-  }
-});
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top-50) {
+                $("nav").css('background-color', 'black');
+                
+            } else {
+                $('nav').css('background-color', 'grey');
+            }
+        });
+    }
+};
+var WIDTH_LIMIT = 860;
 /*add navbar active link*/
-
+const navScrollHighlight=()=>{
+  //$define a jquery var
+  let $allSections=$('.nav_highlight');
+  //user defined scroll function
+  $(window).scroll(function(){
+    let currentScroll=$(this).scrollTop();
+    let $currentSection;
+    let windowWidth = window.innerWidth;
+    $allSections.each(function(){
+      let sectionTop=$(this).offset().top;
+      if(currentScroll > sectionTop && windowWidth>WIDTH_LIMIT){
+        $currentSection=$(this);
+      }
+      let sectionID=$currentSection.attr('id');
+      
+      $('li > a').css('font-weight','400');
+   	  $("[href=#"+sectionID+"]").css('font-weight','800');
+    })
+  });
+};
+$(document).ready(function () {
+  navColorChange();
+  navScrollHighlight();
+});  
 /*add expandable navagation bar when width is less than 860px;*/
